c90184d0b6fa3e0f716ecfca69e2c9aa545543e6
David Belvin <david.belvin@sinclair.edu>
2024-04-11 23:32:54 -0400

    Add project files.

---
+++ CIS.1202.801 - Programming Assignment 15b.cpp
@@ -0,0 +1,21 @@
+/* David Belvin
+CIS.1202.801
+April 17th, 2024
+Professor Bowman
+
+CIS 1202 Programming Assignment #15b
+Templates
+*/
+
+#include <iostream>
+#include <cmath>
+
+template<typename T>
+T half(T value) {
+    return value / 2;
+}
+
+template<>
+int half(int value) {
+    return static_cast<int>(std::round(value / 2.0));
+}
